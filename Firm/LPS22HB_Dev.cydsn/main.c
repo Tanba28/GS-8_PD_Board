7233e02d3a58c691f92e846f6a5047a1be4a5e6e
Tanba28 <wa2.n893@gmail.com>
2020-06-06 00:16:19 +0900

    LPS22HB動かした

--- Firm/LPS22HB_Dev.cydsn/main.c
+++ Firm/LPS22HB_Dev.cydsn/main.c
@@ -15,6 +15,19 @@
 void FreeRtosSetup();
 void FreeRtosStart();
 
+static SemaphoreHandle_t xLPSBinarySemaphor;
+
+CY_ISR(temp){
+    LED_1_Write(~LED_1_Read());
+    BaseType_t xHigherPriorityTaskWoken;
+    
+    xHigherPriorityTaskWoken = pdFALSE;
+    
+    xSemaphoreGiveFromISR(xLPSBinarySemaphor, &xHigherPriorityTaskWoken);
+    
+    portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
+}
+
 void UartEchoBackTask(){ /* Test */
     char buf;    
     
@@ -22,6 +35,24 @@ void UartEchoBackTask(){ /* Test */
         LED_0_Write(~LED_0_Read());
         USBUARTGetString(&buf,1);
         USBUARTPutString(&buf,1);
+    }
+}
+
+void test(){
+    char buf[16];
+    LPS22HB_DATA_CONTEINER_t lps22hb_data;
+    LPS22HBWhoAmI();
+    LPS22HBSetODR(LPS22HB_ODR_25Hz);
+    LPS22HBSetDRDY(1);
+    for(;;){
+        xSemaphoreTake(xLPSBinarySemaphor,portMAX_DELAY);
+        LED_0_Write(~LED_0_Read());
+        taskENTER_CRITICAL();
+        lps22hb_data = LPS22HBUpdateData();
+        taskEXIT_CRITICAL();
+        sprintf(buf,"%7d %f\r\n",Timer_ReadCounter(),LPS22HBGetPress(lps22hb_data));
+        USBUARTPutString(buf,strlen(buf));
+        //vTaskDelay(135);
     }
 }
 
@@ -34,6 +65,10 @@ int main(void)
     USBUARTStart();
     SPIM_LPS_Start();
     
+    Timer_Start();
+    xLPSBinarySemaphor = xSemaphoreCreateBinary();
+    xSemaphoreGive(xLPSBinarySemaphor);
+    isr_1_StartEx(temp);
     FreeRtosStart();
     for(;;)
     {
@@ -54,7 +89,7 @@ void FreeRtosSetup(){
 }
 
 void FreeRtosStart(){
-    xTaskCreate(UartEchoBackTask,"EchoBack",1000,NULL,3,NULL);//Echo back test task
+    xTaskCreate(test,"EchoBack",1000,NULL,3,NULL);//Echo back test task
     
     vTaskStartScheduler();
 }
