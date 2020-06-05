1ea2db7e8e6f0bba8b6484a30c11c6a8d5b433fc
Tanba28 <wa2.n893@gmail.com>
2020-06-05 16:22:37 +0900

    ピンアサイン、FreeRTOSを導入

--- Firm/template.cydsn/main.c
+++ Firm/template.cydsn/main.c
@@ -1,26 +1,63 @@
 /* ========================================
  *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
+ * Copyright (c) 2020 Takumi Niwa
+ * This software is released under the MIT license.
  *
  * ========================================
 */
 #include "project.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "USBUART_FreeRTOS.h"
+
+void FreeRtosSetup();
+void FreeRtosStart();
+
+void UartEchoBackTask(){ /* Test */
+    char buf;    
+    
+    for(;;){     
+        LED_0_Write(~LED_0_Read());
+        USBUARTGetString(&buf,1);
+        USBUARTPutString(&buf,1);
+    }
+}
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    FreeRtosSetup();
+    USBUARTStart();
+    
+    FreeRtosStart();
     for(;;)
     {
         /* Place your application code here. */
+        LED_0_Write(~LED_0_Read());
+        //LED_1_Write(~LED_1_Read());
+        
+        CyDelay(1000);
     }
 }
 
+void FreeRtosSetup(){
+    extern void xPortPendSVHandler( void );
+    extern void xPortSysTickHandler( void );
+    extern void vPortSVCHandler( void );
+    extern cyisraddress CyRamVectors[];
+
+    CyRamVectors[ 11 ] = ( cyisraddress ) vPortSVCHandler;
+    CyRamVectors[ 14 ] = ( cyisraddress ) xPortPendSVHandler;
+    CyRamVectors[ 15 ] = ( cyisraddress ) xPortSysTickHandler;
+}
+
+void FreeRtosStart(){
+    xTaskCreate(UartEchoBackTask,"EchoBack",1000,NULL,3,NULL);//Echo back test task
+    
+    vTaskStartScheduler();
+}
 /* [] END OF FILE */
